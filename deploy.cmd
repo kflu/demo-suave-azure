63a5cf1c688a806e9f46bbbbbd27c60b23173a89
KFL <kfldev@outlook.com>
2016-12-03 22:44:26 -0800

    update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -5,10 +5,14 @@ if not exist .paket (
   mkdir .paket
   curl https://github.com/fsprojects/Paket/releases/download/1.4.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
 
-  .paket\paket.bootstrapper.exe prerelease
   if errorlevel 1 (
     exit /b %errorlevel%
   )
+)
+
+.paket\paket.bootstrapper.exe prerelease
+if errorlevel 1 (
+  exit /b %errorlevel%
 )
 
 if not exist paket.lock (
