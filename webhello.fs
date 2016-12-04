86a00a2bb9b28890440fad692256df6e64fa21b3
KFL <kfldev@outlook.com>
2016-12-03 22:27:56 -0800

    web.config

--- webhello.fs
+++ webhello.fs
@@ -1,8 +1,13 @@
 module webhello
 
 open Suave
-
+open System.Net
+open System.Net.Sockets
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig (Successful.OK "Hello World!")
+    let port = int argv.[0]
+    let config = 
+        { defaultConfig with 
+            bindings = [HttpBinding.mkSimple HTTP "127.0.0.1" port] }
+    startWebServer config (Successful.OK "Hello World!")
     0 // return an integer exit code
